17ba92349888e010e5425770d772523241dece4c
Kaja Limisiewicz <k.limisiewicz@gmail.com>
2021-05-25 15:15:58 +0200

    feat: add CourseGroup to CourseList page

--- frontend-usd/src/pages/CoursesList.js
+++ frontend-usd/src/pages/CoursesList.js
@@ -1,27 +1,9 @@
 import React from "react";
 import { PageHeader, Layout, Divider, Typography } from "antd";
+import CoursesGroup from "../components/CoursesGroup";
 import "../styles/course.less";
 
 const { Content } = Layout;
-const { Link, Text } = Typography;
-
-const mapCourses = (courses) =>
-  courses.map(({ code, time, weekDay, teacher, lessonType, link }) => (
-    <div className="course-list-item">
-      <Text className="course-code">{code}</Text>
-      <Text>{time}</Text>
-      <Text className="long">{weekDay}</Text>
-      <Text className="longer">
-        {teacher.title} {teacher.teacherName}
-      </Text>
-      <Text className="long" style={{ fontWeight: "600" }}>
-        {lessonType}
-      </Text>
-      <Text>
-        <Link to={link}>link</Link>
-      </Text>
-    </div>
-  ));
 
 class CoursesList extends React.Component {
   courses = [
@@ -41,6 +23,19 @@ class CoursesList extends React.Component {
       courseName: "Sztuczna inteligencja",
       lessonType: "laboratorium",
       teacher: {
+        title: "mgr inz.",
+        teacherName: "Jan Kowalski",
+      },
+      time: "15:15",
+      weekDay: "poniedziałek",
+      link: "https://pwr-edu.zoom.us/j/92500313824?pwd=KzNWcmdYV2pabVYzWTh1enEzS003QT09",
+      code: "Z00-00x",
+    },
+    {
+      courseName: "Sztuczna inteligencja",
+      lessonType: "laboratorium",
+      teacher: {
+        title: "mgr inz.",
         teacherName: "Jan Kowalski",
       },
       time: "15:15",
@@ -59,7 +54,10 @@ class CoursesList extends React.Component {
       >
         <Divider />
         <Layout className="site-layout">
-          <Content>{mapCourses(this.courses)}</Content>
+          <Content>
+            <CoursesGroup title="Sztuczna inteligencja" courses={this.courses} />
+            <CoursesGroup title="Hurtownie danych" courses={this.courses} />
+          </Content>
         </Layout>
       </PageHeader>
     );
