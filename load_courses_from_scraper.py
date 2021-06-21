c7d9695f5ee9da50e845f6eb3d677a833b894516
MrMijagi <dawid.bilinski.99@gmail.com>
2021-06-21 15:15:00 +0200

    udpated courses loader

--- load_courses_from_scraper.py
+++ load_courses_from_scraper.py
@@ -37,6 +37,73 @@ def get_teacher_id(teacher_name, access_token):
     return None
 
 
+def get_course_id(course_data, access_token):
+    response = requests.get(COURSES_URL, headers={'Authorization': f'Bearer {access_token}'})
+    json_data = json.loads(response.text)
+
+    for course in json_data:
+        if all([course[key] == course_data[key] for key in course_data.keys()]):
+            return course['id']
+
+    return None
+
+
+def create_course(course, access_token):
+    if course['teacher_id'] != "":
+        teacher = course['teacher_id'].split()
+        teacher = [
+            ' '.join(teacher[:-2]),
+            ' '.join(teacher[-2:])
+        ]
+
+        teacher_id = get_teacher_id(teacher[1], access_token)
+        if teacher_id is None:
+            teacher_id = create_teacher(teacher, access_token)
+    else:
+        teacher_id = None
+
+    payload = {
+        "name": course["name"],
+        "lesson_type": course["lesson_type"],
+        "code": course["code"]
+    }
+
+    if teacher_id is not None:
+        payload["teacher"] = int(teacher_id)
+
+    response = requests.post(COURSES_URL, data=payload, headers={'Authorization': f'Bearer {access_token}'})
+    json_data = json.loads(response.text)
+
+    return json_data['id']
+
+
+def get_lesson_id(lesson_data, course_id, access_token):
+    response = requests.get(f'{COURSES_URL}{course_id}{LESSONS_URL}', headers={'Authorization': f'Bearer {access_token}'})
+    json_data = json.loads(response.text)
+
+    for lesson in json_data:
+        if all([lesson[key] == lesson_data[key] for key in lesson_data.keys()]):
+            return lesson['id']
+
+    return None
+
+
+def create_lesson(lesson, course_id, access_token):
+    lesson_payload = {
+        "when": lesson['when'],
+        "start_time": lesson['start_time'],
+        "end_time": lesson['end_time'],
+        "building": lesson['building'],
+        "room": lesson['room']
+    }
+
+    response = requests.post(f'{COURSES_URL}{course_id}{LESSONS_URL}', data=lesson_payload,
+                  headers={'Authorization': f'Bearer {access_token}'})
+    json_data = json.loads(response.text)
+
+    return json_data['id']
+
+
 def main():
     if len(sys.argv) < 4:
         username = input("username: ")
@@ -53,44 +120,26 @@ def main():
     access_token = get_token(username, password)
 
     for course in courses:
+        course_id = get_course_id({"name": course["name"], "lesson_type": course["lesson_type"]}, access_token)
 
-        if course['teacher_id'] != "":
-            teacher = course['teacher_id'].split()
-            teacher = [
-                ' '.join(teacher[:-2]),
-                ' '.join(teacher[-2:])
-            ]
-
-            teacher_id = get_teacher_id(teacher[1], access_token)
-            if teacher_id is None:
-                teacher_id = create_teacher(teacher, access_token)
-        else:
-            teacher_id = None
-
-        payload = {
-            "name": course["name"],
-            "lesson_type": course["lesson_type"],
-            "code": course["code"]
-        }
-
-        if teacher_id is not None:
-            payload["teacher"] = int(teacher_id)
-
-        response = requests.post(COURSES_URL, data=payload, headers={'Authorization': f'Bearer {access_token}'})
-        json_data = json.loads(response.text)
-        course_id = json_data["id"]
+        if course_id is None:
+            # add new course
+            course_id = create_course(course, access_token)
+            print(f"Created course with id={course_id}")
 
         if course_id is not None:
+            print(f"Going over lessons for course with id={course_id}")
             for lesson in course['lesson']:
-                lesson_payload = {
-                    "when": lesson['when'],
-                    "start_time": lesson['start_time'],
-                    "end_time": lesson['end_time'],
-                    "building": lesson['building'],
-                    "room": lesson['room']
-                }
+                lesson_id = get_lesson_id({
+                    "when": lesson["when"],
+                    "start_time": lesson["start_time"],
+                    "end_time": ["end_time"]
+                }, course_id, access_token)
 
-                requests.post(f'{COURSES_URL}{course_id}{LESSONS_URL}', data=lesson_payload, headers={'Authorization': f'Bearer {access_token}'})
+                if lesson_id is None:
+                    # create new lesson
+                    lesson_id = create_lesson(lesson, course_id, access_token)
+                    print(f'Created lesson with id={lesson_id}')
 
 
 if __name__ == "__main__":
